7321f3f332fc74a6cdfe7c1af94e682864e39a5c
Malcolm Murdock <malcolm.murdock@gmail.com>
2021-08-31 16:17:30 -0700

    wip: functions running

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,10 @@
 // exports.handler = (event, context) => {
+
+// eslint-disable-next-line no-unused-vars
 exports.handler = (event, context) => {
   console.log("hello, World!");
   return {
     statusCode: 200,
-    body: "hello, World!",
+    body: JSON.stringify({ message: "Hello World" }),
   };
 };
